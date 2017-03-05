dcc186a0279226e1a669a6deed4bb53dd9228d19
Tommy Brierly <Tommy Brierly>
2017-03-04 21:32:18 -0500

    test

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,23 @@
-console.log('\'Allo \'Allo!');
+$(document).ready(function(){
+  $('.parallax').parallax();
+  $('.materialboxed').materialbox();
+});
+	
+//Create Chart//
+
+
+
+//Scroll Fire//
+var options = [
+	
+	{selector: '.staggerAniG', offset: 50, callback: function(el) {
+        //Materialize.showStaggeredList($(el));
+      } },
+	  
+	  {selector: '.staggerAniW', offset: 50, callback: function(el) {
+        //Materialize.showStaggeredList($(el));
+      } },
+	
+  ];
+  
+  Materialize.scrollFire(options);
