236f626663b30b28dad7fe1dff2e89bbc0961c7b
Tommy Brierly <Tommy Brierly>
2017-03-06 07:28:26 -0500

    Working d3 chart 2nd try

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -6,149 +6,35 @@ $(document).ready(function(){
 }); 
 
 //Create Chart//
-var data = [ 25, 50, 75, 100 ];
 
-	// graph variables
-	var	xScale				   ,
-		yScale				   ,
-		xAxis				   ,
-		yAxis				   ;
-
-	// chart dimensions
-	var margin   = { top: 20, right: 30, bottom: 20, left: 70 },
-        width    =  $('#chart').width() - margin.left - margin.right,
-        height   =  $(window).height() / 3.5 - margin.top - margin.bottom;
-
-	// color palette for graph
-	var colors = [ '#4398B5', '#ADC4CC', '#92B06A', '#E09D2A', '#DE5F32' ]; 
-
-	// svg object for graph
-	var svg;
-
-	var data;
-
-		$(document).ready(function(){
-
-			drawGraph();
-
-		});
-		
-function drawGraph() {
-
-		xScale = d3.scaleBand()
-				   .domain( d3.range( data.length ) )
-				   .range( [ 0, width ] )
-				   .padding( 0.1 );
-
-		var yMax = d3.max( data );
-
-	    yScale = d3.scaleLinear()
-			   .domain( [ 0, yMax  ] )
-			   .range( [ height, 0 ] );
-
-
-			xAxis = d3.axisBottom( xScale );
-			yAxis = d3.axisLeft( yScale );
-
-			var yTicks 	     = yAxis.ticks(5),
-				yTicksFormat = yAxis.tickFormat( d3.format( ',' ) );
-
-			svg = d3.select( '#chart svg' )
-						.attr( 'width'  , width + margin.left + margin.right  )
-						.attr( 'height' , height + margin.top + margin.bottom )
-						.append( 'g' )
-							.attr( 'transform', 'translate(' + margin.left + ',' + margin.top + ')' );
-
-			svg.selectAll( 'rect' )
-			   .data( data )
-			   .enter()
-			   .append( 'rect' )
-			       .attr('x', function ( d, i ) {
-			       		return xScale( i );
-			       })
-				   .attr('y', function( d, i ) {
-					  	return yScale( 0 );
-				   })
-				   .attr( 'height', function( d ) {
-					   return height - yScale( 0 );
-				   })
-				   .attr( 'width', xScale.bandwidth() )
-				   .attr('fill', function( d, i ) {
-					   return colors[ i % 5 ];
-				   })
-				   .transition()
-				   .duration( 1000 )
-				   .on( 'end', function() {
-					  d3.selectAll( '#chart rect' )
-					  .append( 'title' )
-					  .text( function( d ) {
-						  return d3.format( ',' )( d );
-					  });
-				   })
-				   .attr( 'y', function( d, i ) {
-					   return yScale( d );
-				   })
-				   .attr( 'height', function( d ) {
-					   return height - yScale( d );
-				   });
-
-			svg.append( 'g' )
-			   .attr( 'class', 'yAxis' )
-			   .attr( 'transform', 'translate(0, ' + (-1) + ')')
-			   .call( yAxis );
-
-		}
-
-		$( window ).resize( function() {
-
-			width    = $('#chart').width() - margin.left - margin.right,
-	        height   = $(window).height() / 3.5 - margin.top - margin.bottom;
-
-			xScale.range( [ 0, width ] );
-			d3.select( 'svg' )
-			  .attr( 'width', width + margin.left + margin.right );
-
-			d3.selectAll( '#chart rect' )
-			  .attr( 'x', function( d, i ){
-				  return xScale( i );
-			  })
-			  .attr( 'width', xScale.bandwidth())
-			  .attr( 'fill', function( d, i ) {
-				  return colors[ i % 5 ];
-			  });
-
-		});
-
-	*/	
-	
 function generateData(length) {
   var data = [
   {
-    name: "Html",
+    name: 'Html',
     value: 95
   },
   {
-    name: "Css",
+    name: 'Css',
     value: 95
   },
   {
-    name: "Javascript",
+    name: 'Javascript',
     value: 85
   },
   {
-    name: "jQuery",
+    name: 'jQuery',
     value: 80
   },
   {
-    name: "Php",
+    name: 'Php',
     value: 70
   },
   {
-    name: "Git",
+    name: 'Git',
     value: 70
   },
   {
-    name: "Ajax",
+    name: 'Ajax',
     value: 60
   }
 ];
@@ -203,48 +89,48 @@ var y = d3.scaleLinear()
           .domain([0, d3.max(data, function(d) { return d.value; })]);
    
 // create svg element
-var svg = d3.select("#chart").append("svg")
-  .attr("width", width + margin.left + margin.right)
-  .attr("height", height + margin.top + margin.bottom)
-  .attr("viewBox", "0 0 " + (width + margin.left + margin.right) + " " + (height + margin.top + margin.bottom))
-  .append("g")
-  .attr("transform", 
-        "translate(" + margin.left + "," + margin.top + ")");
+var svg = d3.select('#chart').append('svg')
+  .attr('width', width + margin.left + margin.right)
+  .attr('height', height + margin.top + margin.bottom)
+  .attr('viewBox', '0 0 ' + (width + margin.left + margin.right) + ' ' + (height + margin.top + margin.bottom))
+  .append('g')
+  .attr('transform', 
+        'translate(' + margin.left + ',' + margin.top + ')');
 
 // create x-axis
-var xAxis = svg.append("g")
-  .attr("class", "x-axis axis")
-  .attr("transform", "translate(0," + height + ")")
+var xAxis = svg.append('g')
+  .attr('class', 'x-axis axis')
+  .attr('transform', 'translate(0,' + height + ')')
   .call(d3.axisBottom(x))
-.append("text")
-  .attr("x", width)
-  .attr("y", 30)
-  .attr("dy", ".71em")
-  .style("text-anchor", "end")
-  .text("Skills");
+.append('text')
+  .attr('x', width)
+  .attr('y', 30)
+  .attr('dy', '.71em')
+  .style('text-anchor', 'end')
+  .text('Skills');
 
 // create y-axis
-var yAxis = svg.append("g")
-  .attr("class", "y-axis axis")
-  .call(d3.axisLeft(y).ticks(10, "s"))
-.append("text")
-  .attr("y", -20)
-  .attr("dy", ".71em")
-  .style("text-anchor", "end")
-  .text("Awesomeness Meter");
+var yAxis = svg.append('g')
+  .attr('class', 'y-axis axis')
+  .call(d3.axisLeft(y).ticks(10, 's'))
+.append('text')
+  .attr('y', -20)
+  .attr('dy', '.71em')
+  .style('text-anchor', 'end')
+  .text('Awesomeness Meter');
 
 // create bars
-var bars = svg.selectAll(".bar")
+var bars = svg.selectAll('.bar')
   .data(data)
-  .enter().append("rect")
-  .attr("class", "bar")
-  .attr("x", function(d) { return x(d.name); })
-  .attr("width", x.bandwidth())
-  .attr("y", function(d) { return y(0); })
-	.attr("height", height - y(0))
+  .enter().append('rect')
+  .attr('class', 'bar')
+  .attr('x', function(d) { return x(d.name); })
+  .attr('width', x.bandwidth())
+  .attr('y', function(d) { return y(0); })
+	.attr('height', height - y(0))
 	.transition(t)
-  .attr("y", function(d) { return y(d.value); })
-  .attr("height", function(d) { return height - y(d.value); })
+  .attr('y', function(d) { return y(d.value); })
+  .attr('height', function(d) { return height - y(d.value); })
   .attr('fill', '#0071b9')
   .attr('fill-opacity', function(d) {
     return colorScale(d.value);
